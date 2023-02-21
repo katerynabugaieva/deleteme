6bc91df1f477bb22218f1093d37de62467dc87fa
Kateryna Bugaieva <sonnensmile@gmail.com>
2023-02-21 20:35:36 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const arr = []
+
+  for (let i = 1; i < 5; i++) {
+    arr.push("./img" + i + ".png")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        {arr.map((item, key) => <img src={item} id={key} alt={"дичь" + item} width="100%" />)}
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
